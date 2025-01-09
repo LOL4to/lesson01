31ab22774c3bf545f5ce2bdf175f44c9c5f59218
Ildar Khamzin <idl.08@mail.ru>
2025-01-09 20:49:21 +0900

    new branch lesson01

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "title";
+let screens = "screens";
+let screenPrice = "5000 rub";
+let rollback = 10.4;
+let fullPrice = 30;
+let adaptive = true;
+
+console.log("Message from console");
+alert("Бу! Испугался? Не бойся!");
