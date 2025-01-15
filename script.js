6923c10f0bc88252e29a99724577a01033f8f22f
Ildar Khamzin <idl.08@mail.ru>
2025-01-15 18:52:29 +0900

    fixed, added new values to variables

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
 "use strict";
-let title = "lesson02";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 27;
 const rollback = 40;
-let fullPrice = 150000;
-let adaptive = false;
-
-let screensLow = screens.toLowerCase();
-
-// console.log(typeof title);
-// console.log(typeof fullPrice);
-// console.log(typeof adaptive);
-
-// console.log(screens.length);
-// console.log(
-//   "Стоимость верстки экранов " + screenPrice + " рублей долларов/гривен/юани"
-// );
-// console.log(
-//   "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани"
-// );
-// console.log(screensLow.split());
-// console.log(
-//   "Процент отката посреднику за работу: " + fullPrice * (rollback / 100)
-// );
-
-title = prompt("Как называется ваш проект?"); // спросим в всплыв.окошке и ответ запишем в переменную
-screens = prompt("Какие типы экранов нужно разработать?");
-screenPrice = +prompt("Сколько будет стоить данная работа?");
-adaptive = confirm("Нужен ли адаптив на сайте?"); // вопрос с булевыми ответами
+let title = prompt("Как называется ваш проект?", "lesson03"); // спросим в всплыв.окошке и ответ запишем в переменную
+let screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
+);
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "20000rub");
+let adaptive = confirm("Нужен ли адаптив на сайте?"); // вопрос с булевыми ответами
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let rollbackPerc = fullPrice * (rollback / 100);
 let servicePercentPrice = fullPrice - rollbackPerc;
 
